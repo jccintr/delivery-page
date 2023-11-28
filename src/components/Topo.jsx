61e4bcffe989a2c78b07b3c88695acecd1dbad88
Julio Cesar <jccintr@gmail.com>
2023-11-28 15:45:32 -0300

    sksk

--- src/components/Topo.jsx
+++ src/components/Topo.jsx
@@ -8,13 +8,13 @@ const Topo = () => {
   return (
     <Flex as='section' direction={{ base: 'column', lg: 'row' }} align='center' justify='center' bg='#F0EAFB' pt='50px' pb='50px' px={['0','200px']}>
         
-            <Flex  w={['350px','600px']} direction='column' align='center' justify='center'>
+            <Flex  w={['350px','600px']} direction='column' align='center' justify='center' mb={[10,0]}>
                <Image w={['350px','600px']}   src={notebook} alt="versão pc" />
             </Flex>
 
 
             <Flex  w={['350px','600px']}   direction='column' align='center' justify='flex-start'>
-              <Heading as="h1" fontSize={['2xl','2xl','3xl']}>Tenha o seu próprio aplicativo de delivery, sem taxas ou comissões, a partir de R$ 59,90 mensais. Teste grátis por 15 dias.</Heading>
+              <Heading as="h1" fontSize={['2xl','2xl','3xl']}>Tenha o seu próprio aplicativo de delivery, sem taxas ou comissões, a partir de R$59,90 mensais. Teste grátis por 15 dias.</Heading>
                
                 <Button w={['full','200px']} mt={10} colorScheme='blue'>Quero Assinar</Button>
             </Flex>
